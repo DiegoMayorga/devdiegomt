76598859acfd19fd563aa19a1ef3efdb25aa88cc
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-18 16:39:43 -0500

    Fixes.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+<<<<<<< HEAD
   base: '/',
+=======
+  base: '/diegomayorga.github.io/',
+>>>>>>> 319a640 (Fixes.)
   plugins: [react()],
 })
