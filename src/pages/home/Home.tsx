6b4966b5fe7bff814ad3fc5e8239fa682abefce2
DiegoMayorga <diegoamayorgat@gmail.com>
2025-03-18 18:39:13 -0500

    feat: displaying a random quote in the Home Page.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import classes from "./Home.module.scss";
 
 export default function HomePage() {
+  const [dailyQuote, setDailyQuote] = useState({
+    quote: "",
+    author: "",
+  });
+
+  useEffect(() => {
+    async function fetchDailyPhrase() {
+      const url = "https://api.api-ninjas.com/v1/quotes";
+      const response = await fetch(url, {
+        headers: {
+          "X-Api-Key": "DP6vpE9kshUe3rAzkwxabg==5BiIqc0jG8oiNGnC",
+          "Content-Type": "application/json",
+        },
+      });
+      const resData = await response.json();
+      setDailyQuote(resData[0]);
+    }
+
+    fetchDailyPhrase();
+  }, []);
+
   return (
     <section>
       <h1 className={classes.title}>Home</h1>
-      <div className={classes.profile}>
-        {/* <p>Profilaaaaaaaaaasssssssssssssssssse</p> */}
-      </div>
+      <p>{dailyQuote.quote} - {dailyQuote.author}</p>
+      <div className={classes.profile}></div>
     </section>
   );
 }
